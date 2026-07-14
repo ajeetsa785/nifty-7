457a33fb60292b443b857b29a540697733572146
Ajeet Singh Rathore <ajeetsa785@gmail.com>
2026-07-13 21:30:44 -0700

    complete fintech app overhaul with native bottom nav and transparent pot

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+// Background Service Worker for App Installation
+self.addEventListener('install', (e) => {
+    console.log('Nifty-7 App Service Worker: Installed');
+});
+self.addEventListener('fetch', (e) => {});
